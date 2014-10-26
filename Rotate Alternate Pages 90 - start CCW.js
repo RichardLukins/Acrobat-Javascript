7c901b458ef6c0624de3b715b9af00189f980634
Richard <richard@lukins.co.uk>
2014-10-26 10:10:18 +0000

    Update Rotate Alternate Pages 90 - start CCW.js

--- Rotate Alternate Pages 90 - start CCW.js
+++ Rotate Alternate Pages 90 - start CCW.js
@@ -1,8 +1,22 @@
-    // Add a menu item to collate with another document on the filesystem
+// JavaScript Utilities for Adobe Acrobat (NOT Reader) tested on V8 of Acrobat Pro, should work at least up to V11
+// 
+// Code and Ideas have been borrowed from many sources, these are listed where possible at the end of the Code
+// 
+// Each script can be saved with a .js extension to the javascript directory
+// ie c:
+// you must restart Acrobat after copying to see the menu options change
+// All these utilities are added to the "Edit" Menu
+// to disbale them, remove from the Javascript directory and restart Acrobat
+// Each utility is stand alone, so you can just save each utility as needed
+// You can insert the contents of multiple utilities into a single file, although I prefer to keep them separate for ease of editing
+//
+// Lastly I Have included lots of comments to make any modification easier to facilitate
+// No warranty is given as to operation, and you accept all risks of using this code
+// Backup files before modifying them in case of unexpected results or system crashes
 
-    app.addMenuItem({  cName: "Rotate 90 Alt Pages Odd CCW", cParent: "Edit", cExec: "RotAltPagesOddCCW();",  cEnable: "event.rc = (event.target != null);", nPos: 0 });
+// Add a menu item to collate with another document on the filesystem
+app.addMenuItem({  cName: "Rotate 90 Alt Pages Odd CCW", cParent: "Edit", cExec: "RotAltPagesOddCCW();",  cEnable: "event.rc = (event.target != null);", nPos: 0 });
 
- 
 RotAltPagesOddCCW = app.trustedFunction(function()
 {
     // create a new document
